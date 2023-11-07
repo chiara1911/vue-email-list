74a0d81069ed2b0ac7e4bb089da07b16f64aa33f
chiara1911 <chiaracesari89@gmail.com>
2023-11-07 16:29:38 +0100

    ticket

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      emails: [],
+      result:'',
+      
+    };
+  },
+  methods: {
+    getEmails(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mailhttps://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
+        console.log(resp.data.result),
+       result= resp.data.result,
+       this.emails.push(this.result)
+    })
+    },
+  },
+
+  mounted(){
+
+  },
+}).mount('#app');
